d1f6e0ccfaafc061132157ad663c9f9a0924b678
ngmh <ngmhcartoon@gmail.com>
2025-08-17 02:04:46 +0800

    Echo user input

--- src/main/java/Bernard.java
+++ src/main/java/Bernard.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Bernard {
+    private static Scanner sc = new Scanner(System.in);
+
+    private static String getUserInput() {
+        return sc.nextLine();
+    }
+
     public static void main(String[] args) {
         String logo = " ____                                _ \n" +
                 "| __ )  ___ _ __ _ __   __ _ _ __ __| |\n" +
@@ -8,6 +16,14 @@ public class Bernard {
         System.out.println(logo);
         System.out.println("Hello! I'm Bernard, your helpful companion!");
         System.out.println("How can I help you today?");
+
+        String command = "";
+        boolean ended = false;
+        while (!ended) {
+            command = getUserInput();
+            if (command.equals("bye")) ended = true;
+            System.out.println("> " + command);
+        }
         System.out.println("Goodbye! See you again!");
     }
 }
