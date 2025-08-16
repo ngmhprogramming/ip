d73aecbfc74cd242b91cc9f1424751d336b5b98f
ngmh <ngmhcartoon@gmail.com>
2025-08-17 01:55:21 +0800

    Rename chatbot, add greet and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bernard User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bernard.java
@@ -0,0 +1,13 @@
+public class Bernard {
+    public static void main(String[] args) {
+        String logo = " ____                                _ \n" +
+                "| __ )  ___ _ __ _ __   __ _ _ __ __| |\n" +
+                "|  _ \\ / _ \\ '__| '_ \\ / _` | '__/ _` |\n" +
+                "| |_) |  __/ |  | | | | (_| | | | (_| |\n" +
+                "|____/ \\___|_|  |_| |_|\\__,_|_|  \\__,_|";
+        System.out.println(logo);
+        System.out.println("Hello! I'm Bernard, your helpful companion!");
+        System.out.println("How can I help you today?");
+        System.out.println("Goodbye! See you again!");
+    }
+}
