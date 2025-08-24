fbde017431a7de5882de880a11259b26d4c8eb33
ngmh <ngmhcartoon@gmail.com>
2025-08-24 22:21:42 +0800

    Add Parser class

--- src/main/java/Bernard.java
+++ src/main/java/Bernard.java
@@ -3,16 +3,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Bernard {
-    private static Scanner sc = new Scanner(System.in);
+    private static final String STORAGE_PATH = "./data/bernard.txt";
+
     private static TaskList taskList;
     private static Ui ui = new Ui();
+    private static Parser parser;
+    private static Storage storage;
 
     public static void main(String[] args) {
         ui.title();
 
-        Storage storage = null;
         try {
-            storage = new Storage("./data/bernard.txt");
+            storage = new Storage(STORAGE_PATH);
             taskList = new TaskList(storage.load());
         } catch (BernardException e) {
             ui.outputLine("> ERROR! " + e.getMessage());
@@ -21,53 +23,12 @@ public class Bernard {
 
         ui.greet();
 
+        parser = new Parser(taskList, ui);
         boolean ended = false;
         while (!ended) {
-            String command = ui.getUserInput();
-            String[] commandArgs = command.split(" ");
-            try {
-                if (commandArgs[0].equals("bye")) {
-                    ended = true;
-                    break;
-                } else if (commandArgs[0].equals("list")) {
-                    taskList.listTasks();
-                } else if (commandArgs[0].equals("mark")) {
-                    if (commandArgs.length == 1) {
-                        throw new BernardException("No task specified!");
-                    }
-                    try {
-                        int index = Integer.parseInt(commandArgs[1]) - 1;
-                        taskList.markTask(index);
-                    } catch (NumberFormatException exception) {
-                        throw new BernardException("Invalid task index!");
-                    }
-                } else if (commandArgs[0].equals("unmark")) {
-                    if (commandArgs.length == 1) {
-                        throw new BernardException("No task specified!");
-                    }
-                    try {
-                        int index = Integer.parseInt(commandArgs[1]) - 1;
-                        taskList.unmarkTask(index);
-                    } catch (NumberFormatException exception) {
-                        throw new BernardException("Invalid task index!");
-                    }
-                } else if (commandArgs[0].equals("delete")) {
-                    if (commandArgs.length == 1) {
-                        throw new BernardException("No task specified!");
-                    }
-                    try {
-                        int index = Integer.parseInt(commandArgs[1]) - 1;
-                        taskList.deleteTask(index);
-                    } catch (NumberFormatException exception) {
-                        throw new BernardException("Invalid task index!");
-                    }
-                } else {
-                    taskList.addTask(commandArgs);
-                }
-            } catch (BernardException e) {
-                ui.outputLine("> ERROR! " + e.getMessage());
-            }
+            ended = parser.handleCommand(ui.getUserInput());
         }
+
         try {
             taskList.saveTasks(storage);
         } catch (BernardException e) {
---
+++ src/main/java/Parser.java
@@ -0,0 +1,55 @@
+public class Parser {
+    private TaskList taskList;
+    private Ui ui;
+
+    public Parser(TaskList taskList, Ui ui) {
+        this.taskList = taskList;
+        this.ui = ui;
+    }
+
+    public boolean handleCommand(String command) {
+        String[] commandArgs = command.split(" ");
+        try {
+            if (commandArgs[0].equals("bye")) {
+                return true;
+            } else if (commandArgs[0].equals("list")) {
+                taskList.listTasks();
+            } else if (commandArgs[0].equals("mark")) {
+                if (commandArgs.length == 1) {
+                    throw new BernardException("No task specified!");
+                }
+                try {
+                    int index = Integer.parseInt(commandArgs[1]) - 1;
+                    taskList.markTask(index);
+                } catch (NumberFormatException exception) {
+                    throw new BernardException("Invalid task index!");
+                }
+            } else if (commandArgs[0].equals("unmark")) {
+                if (commandArgs.length == 1) {
+                    throw new BernardException("No task specified!");
+                }
+                try {
+                    int index = Integer.parseInt(commandArgs[1]) - 1;
+                    taskList.unmarkTask(index);
+                } catch (NumberFormatException exception) {
+                    throw new BernardException("Invalid task index!");
+                }
+            } else if (commandArgs[0].equals("delete")) {
+                if (commandArgs.length == 1) {
+                    throw new BernardException("No task specified!");
+                }
+                try {
+                    int index = Integer.parseInt(commandArgs[1]) - 1;
+                    taskList.deleteTask(index);
+                } catch (NumberFormatException exception) {
+                    throw new BernardException("Invalid task index!");
+                }
+            } else {
+                taskList.addTask(commandArgs);
+            }
+        } catch (BernardException e) {
+            ui.outputLine("> ERROR! " + e.getMessage());
+        }
+        return false;
+    }
+}
