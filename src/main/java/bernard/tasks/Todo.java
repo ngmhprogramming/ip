88965bd7f95e465a62aad7d79eeadefd76b81819
ngmh <ngmhcartoon@gmail.com>
2025-08-25 00:24:15 +0800

    Add Javadoc for tasks package

--- src/main/java/bernard/tasks/Todo.java
+++ src/main/java/bernard/tasks/Todo.java
@@ -3,15 +3,30 @@ package bernard.tasks;
 import bernard.exceptions.BernardException;
 
 public class Todo extends Task {
+    /**
+     * Constructs a todo task
+     *
+     * @param description Task description
+     * @throws BernardException If task description is left empty
+     */
     public Todo(String description) throws BernardException {
         super(description);
     }
-
+    /**
+     * Serialises a todo task for file output
+     *
+     * @return The serialised representation of the todo task
+     */
     @Override
     public String serialise() {
         return "T|" + super.serialise();
     }
 
+    /**
+     * Converts a todo task to its string representation
+     *
+     * @return The string representation of the todo task
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
